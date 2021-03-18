f55beff4add1f514cd95f369849d5bfd685937fe
junaid <junaidbaber.cs@gmail.com>
2021-03-18 23:59:20 +0500

    Bulk processing

---
+++ Directory_Processing/cats2gray.m
@@ -0,0 +1,19 @@
+ %this program converts all images in a directory to PNG format
+ % in linux we can all without loading the images using OS executable
+ % commands such as Convert. But we will load every image and then save
+ % into required format
+ inputDir = 'catsjpg/'; % this is the input directory
+ outputDir = 'catsgray/';% after processing all the jpg images will be stored into png format in this directory
+ 
+ loadJPG = dir([inputDir '*.jpg']); % it loads all the files into variable loadJPG
+ 
+ for i=1:length(loadJPG)
+    
+    img = imread([inputDir loadJPG(i).name]); %image is loaded from the inputDir
+    name = loadJPG(i).name; %copies the name of image into variable name
+    fprintf('%d) loading %s \t', i, name);
+    img = rgb2gray(img);
+    imwrite(img, [outputDir name '.png']);
+    fprintf('%d) writing %s .png \n', i, name);
+    
+ end
